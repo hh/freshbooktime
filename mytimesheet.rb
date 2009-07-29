22011deb576ceeb1ddfc5cdab3432f844bc2ff16
Taylor Carpenter <taylor@codecafe.com>
2009-07-28 22:39:57 -0500

    time periods moved to external config

--- mytimesheet.rb
+++ mytimesheet.rb
@@ -4,6 +4,7 @@ require 'yaml'
 apiconfig = YAML.load_file(File.join(File.dirname(__FILE__), "apiconfig.yml"))
 client_config = YAML.load_file(File.join(File.dirname(__FILE__), "client_config.yml"))
 myconfig = YAML.load_file(File.join(File.dirname(__FILE__), "myconfig.yml"))
+timeperiods = YAML.load_file(File.join(File.dirname(__FILE__), "timeperiods.yml"))
 
 t=FreshTime.new(apiconfig)
 
@@ -13,9 +14,7 @@ client_id     = client_config[:catalis][:client_id]
 puts "Timesheet for #{customer_name}:"
 total=0
 weeklist={}
-[ ['2009-07-01','2009-07-04'],
-  ['2009-07-05','2009-07-11'],
-  ['2009-07-12','2009-07-15']].each do |start,stop|
+timeperiods.each do |start,stop|
   ts=t.timesheet_for_client(client_id,start,stop)
   total+=ts[:weekly_total]
   weeklist[[start,stop]]=ts
