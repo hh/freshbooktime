c471a743b71b10222850d79059b7ecee418ed712
Chris McClimans <chris@codecafe.com>
2009-12-29 17:26:20 -0600

    :staff_id is part of the specification, and may allow one to use a single account to edit multiple users time sheets

--- lib/freshbooktime/freshbooks.rb
+++ lib/freshbooktime/freshbooks.rb
@@ -607,12 +607,12 @@ module FreshBooks
     end
   end
 
-  Time_Entry = BaseObject.new(:time_entry_id, :project_id, :task_id, :hours,
+  Time_Entry = BaseObject.new(:time_entry_id, :project_id, :task_id, :staff_id, :hours,
   :notes, :date)
 
   class Time_Entry
     TYPE_MAPPINGS = { 'time_entry_id' => Fixnum, 'project_id' => Fixnum,
-      'task_id' => Fixnum, 'hours' => Float }
+      'staff_id' => Fixnum, 'task_id' => Fixnum, 'hours' => Float }
 
     def create
       resp = FreshBooks::call_api('time_entry.create', 'time_entry' => self)
